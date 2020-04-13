c7faac8f6bb3b4975ce2486da18c341d74f033b0
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-04-13 20:03:44 +0900

    fix bugs

--- _literate/example.jl
+++ _literate/example.jl
@@ -3,6 +3,10 @@
 
 using Plots
 Plots.plot(sin)
+x = -π:0.01:π
+y = sin.(x)
+plot(x, y)
 Plots.savefig(joinpath(@OUTPUT, "plot.png")) # hide
 
+# <!-- To show the result, we need state \fig command -->
 # \fig{plot}
