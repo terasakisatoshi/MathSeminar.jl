468ce7b866eeb71d242ca92591d5382ce823be8e
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-04-24 21:49:52 +0900

    fix deploy.jl

--- _mytools/deploy.jl
+++ _mytools/deploy.jl
@@ -1,11 +1,11 @@
 using Pkg
-using NodeJS
-using Franklin
-using Remark
 
 Pkg.add(["NodeJS", "Franklin", "Literate", "Remark"])
 Pkg.add(["WebIO", "Plotly", "PlotlyJS", "ORCA"])
 run(`$(npm_cmd()) install highlight.js`)
+
+using NodeJS, Franklin, Remark
+
 Pkg.activate(".")
 Pkg.instantiate()
 Remark.slideshow("./remark/example.jl", "./remark")
