66c90a01409d7cf24bcbe9d278e02499ff7d1300
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2020-04-24 22:05:38 +0900

    fix

--- _mytools/deploy.jl
+++ _mytools/deploy.jl
@@ -1,12 +1,21 @@
+# This ensures that NodeJS and Franklin are loaded then it installs
+# highlight.js which is needed for the prerendering step.
+# Then the environment is activated and instantiated to install all
+# Julia packages which may be required to successfully build your site.
+# NOTE: the last line should be `optimize()`, you may want to give it
+# specific arguments, see the documentation or ?optimize in the REPL.
+
 using Pkg
 
 Pkg.add(["NodeJS", "Franklin", "Literate", "Remark"])
 Pkg.add(["WebIO", "Plotly", "PlotlyJS", "ORCA"])
-run(`$(npm_cmd()) install highlight.js`)
 
 using NodeJS, Franklin, Remark
 
+run(`$(npm_cmd()) install highlight.js`)
+
 Pkg.activate(".")
 Pkg.instantiate()
+
 Remark.slideshow("./remark/example.jl", "./remark")
 optimize(minify = false)
