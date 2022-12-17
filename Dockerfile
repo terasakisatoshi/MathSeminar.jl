61ad21f81f43b3e35cfa52a783df4a1cc3da1597
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2022-12-17 19:44:48 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN apt-get update && \
     texlive-latex-recommended  \
     zip \
     libxt6 libxrender1 libxext6 libgl1-mesa-glx libqt5widgets5 # GR && \
-    apt-get clean && rm -rf /var/lib/apt/lists/* # clean up    
+    apt-get clean && rm -rf /var/lib/apt/lists/* # clean up
 
 # Setup Rust
 # Reference: https://github.com/rust-lang/rustup/issues/297
@@ -24,12 +24,12 @@ ENV R_HOME="*"
 ENV PYTHON=""
 
 RUN julia -e 'using Pkg; \
-Pkg.add(["PyCall", "Conda"]); using PyCall, Conda;\
-using Conda; Conda.add(["matplotlib", "sympy", "numpy", "numba" ]); \
-Pkg.add(["Documenter", "Literate", "Weave", "Franklin", "NodeJS", "Remark"]); \
-'
+    Pkg.add(["PyCall", "Conda"]); using PyCall, Conda;\
+    using Conda; Conda.add(["matplotlib", "sympy", "numpy", "numba" ]); \
+    Pkg.add(["Documenter", "Literate", "Weave", "Franklin", "NodeJS", "Remark"]); \
+    '
 
-RUN julia -e "using NodeJS; run(\`\$(npm_cmd()) install highlight.js\`); using Franklin"
+RUN julia -e "using NodeJS; run(\`\$(npm_cmd()) install highlight.pack.js\`); using Franklin"
 
 # Add conda's path
 ENV PATH /root/.julia/conda/3/bin:$PATH
@@ -37,7 +37,7 @@ ENV PATH /root/.julia/conda/3/bin:$PATH
 #RUN	conda install r-base && \
 #    julia -e 'using Pkg; pkg"add RCall"; Pkg.build("RCall"); using RCall'
 
-# set "/work" as default project directory 
+# set "/work" as default project directory
 WORKDIR /work
 ENV JULIA_PROJECT=/work
 
@@ -47,10 +47,10 @@ COPY ./src/MyUtils.jl /work/src/MyUtils.jl
 
 # Initialize Julia package using /work/Project.toml
 RUN rm -f /work/Manifest.toml && julia --project=/work -e 'using Pkg; \
-Pkg.instantiate(); \
-Pkg.precompile()' && \
-# Check Julia version \
-julia -e 'using InteractiveUtils; versioninfo()'
+    Pkg.instantiate(); \
+    Pkg.precompile()' && \
+    # Check Julia version \
+    julia -e 'using InteractiveUtils; versioninfo()'
 
 # For Http Server
 EXPOSE 8000
--- _mytools/deploy.jl
+++ _mytools/deploy.jl
@@ -1,5 +1,5 @@
 # This ensures that NodeJS and Franklin are loaded then it installs
-# highlight.js which is needed for the prerendering step.
+# highlight.pack.js which is needed for the prerendering step.
 # Then the environment is activated and instantiated to install all
 # Julia packages which may be required to successfully build your site.
 # NOTE: the last line should be `optimize()`, you may want to give it
@@ -8,7 +8,7 @@
 using Pkg
 using NodeJS
 
-run(`$(npm_cmd()) install highlight.js`)
+run(`$(npm_cmd()) install highlight.pack.js`)
 
 Pkg.build("Franklin")
 
@@ -18,16 +18,16 @@ Pkg.activate(".")
 Pkg.instantiate()
 
 # make slide
-Remark.slideshow("./slideshow/remark", options = Dict("ratio" => "16:9"), title = "Remark.jl")
-Remark.slideshow("./slideshow/franklin", options = Dict("ratio" => "16:9"), title = "Franklin.jl")
-Remark.slideshow("./slideshow/dash", options = Dict("ratio" => "16:9"), title = "Dash.jl")
-Remark.slideshow("./slideshow/binarybuilder", options = Dict("ratio" => "16:9"), title = "BinaryBuilder.jl")
-Remark.slideshow("./slideshow/myworkflow", options = Dict("ratio" => "16:9"), title = "MyWorkflow.jl")
-Remark.slideshow("./slideshow/repl", options = Dict("ratio" => "16:9"), title = "Do something great in your REPL")
-Remark.slideshow("./slideshow/intro2julia", options = Dict("ratio" => "16:9"), title = "Intro2Julia")
-Remark.slideshow("./slideshow/pycallchainrules", options = Dict("ratio" => "16:9"), title = "PyCallChainRules.jl")
-Remark.slideshow("./slideshow/replay", options = Dict("ratio" => "16:9"), title = "Replay.jl")
-Remark.slideshow("./slideshow/pluto", options = Dict("ratio" => "16:9"), title = "Intro to Pluto.jl")
+Remark.slideshow("./slideshow/remark", options=Dict("ratio" => "16:9"), title="Remark.jl")
+Remark.slideshow("./slideshow/franklin", options=Dict("ratio" => "16:9"), title="Franklin.jl")
+Remark.slideshow("./slideshow/dash", options=Dict("ratio" => "16:9"), title="Dash.jl")
+Remark.slideshow("./slideshow/binarybuilder", options=Dict("ratio" => "16:9"), title="BinaryBuilder.jl")
+Remark.slideshow("./slideshow/myworkflow", options=Dict("ratio" => "16:9"), title="MyWorkflow.jl")
+Remark.slideshow("./slideshow/repl", options=Dict("ratio" => "16:9"), title="Do something great in your REPL")
+Remark.slideshow("./slideshow/intro2julia", options=Dict("ratio" => "16:9"), title="Intro2Julia")
+Remark.slideshow("./slideshow/pycallchainrules", options=Dict("ratio" => "16:9"), title="PyCallChainRules.jl")
+Remark.slideshow("./slideshow/replay", options=Dict("ratio" => "16:9"), title="Replay.jl")
+Remark.slideshow("./slideshow/pluto", options=Dict("ratio" => "16:9"), title="Intro to Pluto.jl")
 
 weave(joinpath("./programming", "weave.jmd"), out_path="./programming", doctype="github")
 optimize(minify=false, prepath="MathSeminar.jl", clear=true)
